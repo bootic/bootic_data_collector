fdcaab7912e79819ac1d7ddf360bde70cb1e4c1b
Ismael Celis <ismaelct@gmail.com>
2012-10-11 20:59:04 +0100

    Client sends JSON event

--- client.rb
+++ client.rb
@@ -1,11 +1,17 @@
 require 'socket'
+require 'json'
 s = UDPSocket.new
 
 # HOST = '192.168.245'
 HOST = 'localhost'
 
 1.upto 10 do |i|
-  msg = "hello ##{i}"
+  msg = JSON.dump(
+    event_name: "hello ##{i}",
+    payload: {
+      date: Time.now.to_s
+    }
+  )
   s.send(msg, 0, HOST, 5555)  
   p [:send, msg]
   sleep 0.5
