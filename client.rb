f555ad1af8f542aeafc1d66a915de0ebd3a6e087
Sam Taylor <sjltaylor@gmail.com>
2012-10-12 17:22:06 +0100

    Working

--- client.rb
+++ client.rb
@@ -5,12 +5,16 @@ s = UDPSocket.new
 # HOST = '192.168.245'
 HOST = 'localhost'
 
-1.upto 10 do |i|
+N = 70
+
+tags = (1..10).map {|i| "tag-#{i}"}
+
+1.upto N do |i|
   msg = JSON.dump(
     desc: "hello ##{i}",
-    tags: ['tag1', 'tag2', 'tag3', "tag-x-#{i}"]
+    tags: tags.shuffle.take(3)
   )
   s.send(msg, 0, HOST, 5555)  
   p [:send, msg]
-  sleep 0.5
+  sleep 0.1
 end
--- web/router.go
+++ web/router.go
@@ -4,14 +4,26 @@ import (
 	"code.google.com/p/gorilla/mux"
 	"text/template"
 	"net/http"
+	//"fmt"
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/", root)
+	router.HandleFunc("/", tmpl("views/home.html"))
+	router.HandleFunc("/realtime_ftw", tmpl("views/realtime.html"))
+	router.HandleFunc("/app.js", javascript("js/app.js"))
 	return router
 }
 
-func root (res http.ResponseWriter, req *http.Request) {
-	template.Must(template.ParseFiles("views/home.html")).Execute(res, req.Host)
+func tmpl(name string) (handle func (http.ResponseWriter, *http.Request)) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		template.Must(template.ParseFiles(name)).Execute(res, req.Host)
+	}
+}
+
+func javascript(name string) (handle func (http.ResponseWriter, *http.Request)) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Add("Content-Type", "application/javascript")
+		template.Must(template.ParseFiles(name)).Execute(res, req.Host)
+	}
 }
