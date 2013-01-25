6c7c7c5aab1973917ce77e8bd700ed711487c2a4
Ismael Celis <ismaelct@gmail.com>
2013-01-25 17:04:55 +0000

    Push events to ZMQ PUB/SUB socket

---
+++ fanout/observer.go
@@ -0,0 +1,53 @@
+package fanout
+
+import (
+  "datagram.io/data"
+  zmq "github.com/alecthomas/gozmq"
+  "bytes"
+)
+
+type ZMQObserver struct {
+  socket zmq.Socket
+  Notifier data.EventsChannel
+}
+
+func (observer *ZMQObserver) listen () {
+  for {
+    event := <- observer.Notifier
+    evtBytes, err := event.MarshalJSON()
+    if err != nil {
+      break
+    }
+    evtType, _ := event.Get("type").String()
+    
+    observer.dispatch(evtType, evtBytes)
+    observer.dispatch("all", evtBytes)
+  }
+}
+
+func (observer *ZMQObserver) dispatch(topicStr string, evtBytes []byte) {
+  
+  topic := []byte(topicStr)
+  
+  a := [][]byte{ topic, evtBytes } 
+  
+  topic_and_message := bytes.Join(a, []byte{' '})
+  
+  observer.socket.Send(topic_and_message, 0)
+}
+
+func NewZmq(host string) (observer *ZMQObserver) {
+  context, _ := zmq.NewContext()
+  socket, _ := context.NewSocket(zmq.PUB)
+  
+  socket.Bind(host)
+  
+  observer = &ZMQObserver{
+    socket: socket,
+    Notifier: make(data.EventsChannel, 1),
+  }
+  
+  go observer.listen()
+  
+  return
+}
--- main.go
+++ main.go
@@ -4,18 +4,16 @@ import (
   "net/http"
   "datagram.io/udp"
   "datagram.io/udp/ws"
-  // "datagram.io/db"
-  "datagram.io/redis_stats"
+  "datagram.io/fanout"
   "log"
   "os"
 )
 
-func daemons() (err error) {
+func main() {
   // Configure via env variables
   // WS and UDP hosts can be different, ex. UDP could be listening on a private IP while WS is public
   udp_host  := os.Getenv("DATAGRAM_IO_UDP_HOST")
   ws_host   := os.Getenv("DATAGRAM_IO_WS_HOST")
-  redis_host:= os.Getenv("REDIS_HOST")
   
   // Start up UDP daemon +++++++++++++++++++++++++++++++++++++++++++++++
   daemon, err := udp.NewDaemon(udp_host)
@@ -23,36 +21,18 @@ func daemons() (err error) {
     panic(err)
   }
   
-  // Track some event types to redis +++++++++++++++++++++++++++++++++++
-  if redis_host != "" {
-    tracker, err := redis_stats.NewTracker(redis_host)
-
-    if err != nil {
-      panic(err)
-    }
-    
-    log.Println("Using redis", redis_host)
-    // Track pageview events as redis increment time series
-    daemon.SubscribeToType(tracker.Notifier, "pageview")
-    daemon.SubscribeToType(tracker.Funnels, "order")
-  }
+  // Setup Websockets hub ++++++++++++++++++++++++++++++++++++++++++++++
+  wshub := ws.HandleWebsocketsHub("/ws")
+  log.Println("websocket server at " + ws_host + "/ws")
   
-	// Setup Websockets hub ++++++++++++++++++++++++++++++++++++++++++++++
-	wshub := ws.HandleWebsocketsHub("/ws")
-	log.Println("websocket server at " + ws_host + "/ws")
-
-	// Push incoming UDP messages to multiple listeners ++++++++++++++++++
-	// Push all events
+  // Push incoming UDP messages to multiple listeners ++++++++++++++++++
+  // Push all events
   daemon.Subscribe(wshub.Notifier)
-  // We can also filter events by type
+  
+  fanoutObserver := fanout.NewZmq("tcp://127.0.0.1:6000")
+  daemon.Subscribe(fanoutObserver.Notifier)
+  log.Println("ZMQ fanout at tcp://127.0.0.1:6000")
+  
 
-	log.Fatal("HTTP server error: ", http.ListenAndServe(ws_host, nil))
-
-	return nil
-}
-
-func main() {
-  if err := daemons(); err != nil {
-    log.Println(err)
-  }
+  log.Fatal("HTTP server error: ", http.ListenAndServe(ws_host, nil))
 }
